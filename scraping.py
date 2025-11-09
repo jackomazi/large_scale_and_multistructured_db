acf5567eb849a85c26838d0ba68117f2a4e10a75
jackomazi <andreagiacomazzi202@gmail.com>
2025-11-09 13:59:59 +0100

    Aggiunto scraping da lichess

--- scraping.py
+++ scraping.py
@@ -3,6 +3,8 @@ from pymongo import MongoClient
 import logging
 import logging.config
 import sys
+import json
+from time import sleep
 
 logging.config.fileConfig('log/logger.config')
 logger = logging.getLogger('scraping')
@@ -20,7 +22,24 @@ except Exception as e:
     logger.info("="*50)
     sys.exit(1)
 
-def fetch_chess_com_games(username, year, month):
+def format_chess_com_game(game: dict) -> dict:
+    return {
+        "url": game.get("url"),
+        "white_player": game.get("white", {}).get("username"),
+        "black_player": game.get("black", {}).get("username"),
+        "white_rating": game.get("white", {}).get("rating"),
+        "black_rating": game.get("black", {}).get("rating"),
+        "result_white": game.get("white", {}).get("result"),
+        "result_black": game.get("black", {}).get("result"),
+        "eco_url": game.get("eco"),
+        "opening": game.get("eco", "").split("/")[-1] if game.get("eco") else None,
+        "pgn": game.get("pgn"),
+        "time_class": game.get("time_class"),
+        "rated": game.get("rated"),
+        "end_time": game.get("end_time"),
+    }
+
+def fetch_chess_com_games(username: str, year: int, month: int) -> list:
     url = f"https://api.chess.com/pub/player/{username}/games/{year}/{month:02d}"
     logger.info(f"Fetching games for {username} for {year}-{month:02d} from {url}")
     headers = {
@@ -40,12 +59,65 @@ def fetch_chess_com_games(username, year, month):
         logger.info("="*50)
         sys.exit(1)
 
-def save_games_to_db(games):
+def format_lichess_game(game: dict) -> dict:
+    return {
+        "url": game.get("url"),
+        "white_player": game.get("players", {}).get("white", {}).get("user", {}).get("name"),
+        "black_player": game.get("players", {}).get("black", {}).get("user", {}).get("name"),
+        "white_rating": game.get("players", {}).get("white", {}).get("rating"),
+        "black_rating": game.get("players", {}).get("black", {}).get("rating"),
+        "result_white": game.get("players", {}).get("white", {}).get("result"),
+        "result_black": game.get("players", {}).get("black", {}).get("result"),
+        "eco_url": game.get("opening", {}).get("eco"),
+        "opening": game.get("opening", {}).get("name"),
+        "pgn": game.get("pgn"),
+        "time_class": game.get("speed"),
+        "rated": game.get("rated"),
+        "end_time": game.get("end_time"),
+    }
+
+
+def fetch_lichess_games(username: str, year: str, month: str) -> list:
+    url = f"https://lichess.org/api/games/user/{username}?since={year}-{month:02d}-01T00:00:00Z&until={year}-{month:02d}-31T23:59:59Z"
+    logger.info(f"Fetching games for {username} for {year}-{month:02d} from {url}")
+    headers = {"Accept": "application/x-ndjson"}
+    try:
+        response = requests.get(url, headers=headers, stream =True)
+    except Exception as e:
+        logger.error(f"Error fetching games for {username} for {year}-{month:02d}: {e}")
+        logger.info("="*50)
+        sys.exit(1)
+    
+    games = []
+    if response.status_code == 200:
+        sleep(1)
+        for line in response.iter_lines():
+            if line:
+                game = json.loads(line)
+                games.append(game)
+        logger.info(f"Fetched {len(games)} games for {username} for {year}-{month:02d}")
+        return games
+    else:
+        logger.error(f"Failed to fetch games for {username} for {year}-{month:02d}")
+        logger.error(f"Status Code: {response.status_code}, Response: {response.text}")
+        logger.info("="*50)
+        sys.exit(1)
+
+def save_games_to_db(games: str, formatter: callable) -> None:
+    for g in games:
+        game_doc = formatter(g)
+        collection.update_one(
+            {"url": game_doc["url"]},
+            {"$set": game_doc},
+            upsert=True
+        )
+
+
+def save_games_to_db_chess_com(games: list) -> None:
     if not games:
         logger.info("No games to save to the database.")
         return
     for g in games:
-        
         game_doc = {
             "url": g.get("url"),
             "white_player": g.get("white", {}).get("username"),
@@ -71,10 +143,28 @@ def save_games_to_db(games):
 
 
 if __name__ == "__main__":
-    username = "jack_o_mazi"
-    year = 2025
-    month = 10
-    games = fetch_chess_com_games(username, year, month)
-    save_games_to_db(games)
+    chess_com_users = ["jack_o_mazi", "jeccabahug"]
+    lichess_users = ["MagnusCarlsen"]
+    
+    for username in chess_com_users:
+        logger.info("-"*50)
+        logger.info(f"Starting to process player: {username}")
+        for year in [2024]:
+            logger.info(f"Processing year: {year}")
+            for month in range(1, 5):
+                logger.info(f"Processing month: {month:02d}")
+                games = fetch_chess_com_games(username, year, month)
+                save_games_to_db(games, format_chess_com_game)
+    
+    for username in lichess_users:
+        logger.info("-"*50)
+        logger.info(f"Starting to process player: {username}")
+        for year in [2024]:
+            logger.info(f"Processing year: {year}")
+            for month in range(1, 5):
+                logger.info(f"Processing month: {month:02d}")
+                games = fetch_lichess_games(username, year, month)
+                save_games_to_db(games, format_lichess_game)
+    
     logger.info("Scraping and saving process completed.")
     logger.info("="*50)

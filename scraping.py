36304655a6a4b0841be384886919876f2092db2d
jackomazi <andreagiacomazzi202@gmail.com>
2025-11-09 16:20:08 +0100

    Chiave pgn rimossa e sostituita con moves

--- scraping.py
+++ scraping.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import calendar
 import time
+import re
 
 logging.config.fileConfig('log/logger.config')
 logger = logging.getLogger('scraping')
@@ -26,6 +27,23 @@ except Exception as e:
     logger.info("="*50)
     sys.exit(1)
 
+def extract_moves_from_pgn(pgn: str) -> str:
+    lines = pgn.splitlines()
+    moves = []
+    for line in lines:
+        # salta tutte le righe che iniziano con [
+        if not line.startswith("["):
+            moves.append(line.strip())
+    moves_str = " ".join(moves)
+    # rimuove i clock {[%clk ...]} e altri commenti
+    moves_str = re.sub(r"\{.*?\}", "", moves_str)
+    # rimuove numeri di mossa come "1." o "1..."
+    moves_str = re.sub(r"\d+\.+", "", moves_str)
+    # normalizza spazi
+    moves_str = " ".join(moves_str.split())
+    return moves_str
+
+
 def format_chess_com_game(game: dict) -> dict:
     return {
         "url": game.get("url"),
@@ -37,7 +55,7 @@ def format_chess_com_game(game: dict) -> dict:
         "result_black": game.get("black", {}).get("result"),
         "eco_url": game.get("eco"),
         "opening": game.get("eco", "").split("/")[-1] if game.get("eco") else None,
-        "pgn": game.get("pgn"),
+        "moves": extract_moves_from_pgn(game.get("pgn", "")),
         "time_class": game.get("time_class"),
         "rated": game.get("rated"),
         "end_time": game.get("end_time"),
@@ -63,29 +81,12 @@ def fetch_chess_com_games(username: str, year: int, month: int) -> list:
         logger.info("="*50)
         sys.exit(1)
 
-#def format_lichess_game(game: dict) -> dict:
-#    print(game)
-#    return {
-#        "url": game.get("url"),
-#        "white_player": game.get("players", {}).get("white", {}).get("user", {}).get("name"),
-#        "black_player": game.get("players", {}).get("black", {}).get("user", {}).get("name"),
-#        "white_rating": game.get("players", {}).get("white", {}).get("rating"),
-#        "black_rating": game.get("players", {}).get("black", {}).get("rating"),
-#        "result_white": game.get("players", {}).get("white", {}).get("result"),
-#        "result_black": game.get("players", {}).get("black", {}).get("result"),
-#        "eco_url": game.get("opening", {}).get("eco"),
-#        "opening": game.get("opening", {}).get("name"),
-#        "pgn": game.get("pgn"),
-#        "time_class": game.get("speed"),
-#        "rated": game.get("rated"),
-#        "end_time": game.get("end_time"),
-#    }
+
 def format_lichess_game(game: dict) -> dict:
     white = game.get("players", {}).get("white", {})
     black = game.get("players", {}).get("black", {})
     winner = game.get("winner")
-    if game.get("speed") == "classical":
-        print(game)
+    
     return {
         "url": f"https://lichess.org/{game.get('id')}",
         "white_player": white.get("user", {}).get("name"),
@@ -96,7 +97,7 @@ def format_lichess_game(game: dict) -> dict:
         "result_black": "win" if winner == "black" else "loss" if winner == "white" else "draw",
         "eco_url": game.get("opening", {}).get("eco"),
         "opening": game.get("opening", {}).get("name"),
-        "pgn": game.get("moves"),
+        "moves": game.get("moves"),
         "time_class": game.get("speed"),
         "rated": game.get("rated"),
         "end_time": datetime.fromtimestamp(game.get("lastMoveAt")/1000) if game.get("lastMoveAt") else None
@@ -110,7 +111,7 @@ def fetch_lichess_games(username: str, year: str, month: str) -> list:
     since = int(calendar.timegm(start_day.timetuple()) * 1000)
     until = int(calendar.timegm(end_date.timetuple()) * 1000)
 
-    url = f"https://lichess.org/api/games/user/{username}?since={since}&until={until}&max=300&format=ndjson&opening=true"
+    url = f"https://lichess.org/api/games/user/{username}?since={since}&until={until}&max=300&format=ndjson&opening=true&pgnInJson=true"
     logger.info(f"Fetching games for {username} for {year}-{month:02d} from {url}")
     headers = {"Accept": "application/x-ndjson"}
     try:
@@ -141,36 +142,6 @@ def save_games_to_db(games: str, formatter: callable) -> None:
         return
     for g in games:
         game_doc = formatter(g)
-        collection.update_one(
-            {"url": game_doc["url"]},
-            {"$set": game_doc},
-            upsert=True
-        )
-    logger.info(f"Saved {len(games)} games to the database.")
-
-
-
-def save_games_to_db_chess_com(games: list) -> None:
-    if not games:
-        logger.info("No games to save to the database.")
-        return
-    for g in games:
-        game_doc = {
-            "url": g.get("url"),
-            "white_player": g.get("white", {}).get("username"),
-            "black_player": g.get("black", {}).get("username"),
-            "white_rating": g.get("white", {}).get("rating"),
-            "black_rating": g.get("black", {}).get("rating"),
-            "result_white": g.get("white", {}).get("result"),
-            "result_black": g.get("black", {}).get("result"),
-            "eco_url": g.get("eco"),
-            "opening": g.get("eco", "").split("/")[-1] if g.get("eco") else None,
-            "pgn": g.get("pgn"),
-            "time_class": g.get("time_class"),
-            "rated": g.get("rated"),
-            "end_time": g.get("end_time"),
-        }
-        
         collection.update_one(
             {"url": game_doc["url"]},
             {"$set": game_doc},
